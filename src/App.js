d6426e68758a5e0ee3375fe14cae5fb479a06ba8
raphamsantos <raphamndds@gmail.com>
2023-05-13 15:30:01 -0300

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import  "./style.css"
+import alface from "./assets/alface.png";
+import beterraba from "./assets/beterraba.png";
+import cenoura from "./assets/cenoura.png";
+import laranja from "./assets/laranja.png";
+import limao from "./assets/limao.png";
+import manga from "./assets/manga.png";
+import tomate from "./assets/tomate.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    titulo: "HORTIFRUTI",
+    subtitulo: "Vnw",
+    compras: [
+      {
+        produto: `${alface}`
+      },
+      {
+        produto: `${laranja}`
+      },
+      {
+        produto: `${cenoura}`
+      },
+      {
+        produto: `${manga}`
+      },
+      {
+        produto: `${limao}`
+      },
+      {
+        produto: `${beterraba}`
+      },
+      {
+       produto: `${tomate}`
+      }
+    ]
+  };
+  render() {
+    return (
+      <body>
+        <header>
+          <div>
+            <h1>{this.state.titulo}</h1>
+            <h2>{this.state.subtitulo}</h2>
+          </div>
+        </header>
+        <main>
+          <h3>NOSSOS PRODUTOS</h3>
+          <section>
+            <div>
+            {this.state.compras.map((item)=>(
+            <img className="imagens" src={item.produto}/>
+            ))}
+            </div>
+          </section>
+        </main>
+        <aside>
+          <section>
+            <div class="carrinho">
+              <p>Arraste sua compra para colocar no carrinho</p>
+              <p>🛒</p>
+            </div>
+            <div class="quantidade">
+              <p>+</p>
+              <p>-</p>
+            </div>
+          </section>
+        </aside>
+      </body>
+    );
+  }
 }
-
-export default App;
